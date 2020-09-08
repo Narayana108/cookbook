8af484c68e078437087ad149e0178ec29a23a65e
Narayana108 <narayan-das@tuta.io>
2020-09-08 20:50:33 +0200

    python

---
+++ python/functions/functions.py
@@ -0,0 +1,18 @@
+def hello(name):
+  print("Ahoi " + name)
+
+hello("Jahman")
+
+
+def country(home = "Ethopia"):
+  print("I am from " + home)
+
+country()
+country("Egypt")
+
+
+def sum_of_two_numbers(a,b):
+  return a + b
+
+x = sum_of_two_numbers(1,2)
+print(x)
---
+++ python/string_operations/string.py
@@ -0,0 +1,12 @@
+astring = "Namaste World!"
+
+print(len(astring))
+print(astring.index("a"))
+print(astring.count("a"))
+print(astring[3:6])
+print(astring[0:15:2])
+
+print(astring.startswith("Namast"))
+print(astring.endswith("Ahoi"))
+
+print(astring.split(" "))
