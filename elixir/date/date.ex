52b206e7036a0818b56f1a03bcf8f0d27916ebac
narayana108 <narayan-das@github.com>
2021-07-13 20:41:17 +0530

    Elixir
    Signed-off-by: narayana108 <narayan-das@github.com>

--- elixir/date/date.ex
+++ elixir/date/date.ex
@@ -2,4 +2,18 @@
 
 time = Time.utc_now
 IO.puts time
-IO.puts(time.hour <> time.minute)
+IO.puts("#{time.hour}, #{time.minute}")
+
+# Date
+IO.puts Date.utc_today
+{:ok, date} = Date.new(2020, 07, 21)
+IO.puts date
+IO.puts(Date.leap_year? date)
+
+# NaiveDateTime - No timezone support
+IO.puts NaiveDateTime.utc_now
+
+# DateTime
+IO.puts DateTime.utc_now
+
+
